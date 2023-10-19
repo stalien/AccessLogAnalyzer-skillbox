69766a6c1b3baa0b0fdffdb8a5df6e0daac6badf
stalien <stalien@yandex.ru>
2023-10-19 14:52:26 +0300

    read all lines from log file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,20 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private static final String path = "/Users/stalien/Desktop/access.log";
+
+    public static void main( String[] args ) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(path));
+
+        for(String line : lines) {
+            System.out.println(line);
+        }
     }
 }
