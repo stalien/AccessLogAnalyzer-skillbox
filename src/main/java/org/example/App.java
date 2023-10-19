229aef9456cc6b0ebd2147499d0efdc07816119e
stalien <stalien@yandex.ru>
2023-10-19 18:45:06 +0300

    parse dateTime from string

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,23 +5,51 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App 
 {
 
     private static final String path = "/Users/stalien/Desktop/access.log";
+    private static DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z").withLocale(Locale.ENGLISH);
+    private static String dateTimeRegex = ".+\\[([^;\\]]+)\\].+";
 
     public static void main( String[] args ) throws IOException {
-//        List<String> lines = Files.readAllLines(Paths.get(path));
-//
-//        for(String line : lines) {
-//            System.out.println(line);
-//        }
-        Statistics statistics = new Statistics(40, 12);
-        Gson gson = new Gson();
-        String json = gson.toJson(statistics);
 
-        System.out.println(json);
+//        String dateTime = "15/Aug/2023:06:25:15 +0300";
+//        System.out.println(getTimestamp(dateTime));
+
+        List<String> lines = Files.readAllLines(Paths.get(path));
+
+//        System.out.println(lines.get(0));
+
+        Pattern dateTimePattern = Pattern.compile(dateTimeRegex);
+
+        for (String line : lines) {
+            Matcher matcher = dateTimePattern.matcher(line);
+            if (!matcher.find()) {
+                continue;
+            }
+            String dateTime = matcher.group(1);
+            System.out.println(dateTime);
+        }
+//        Statistics statistics = new Statistics(40, 12);
+//        Gson gson = new Gson();
+//        String json = gson.toJson(statistics);
+//
+//        System.out.println(json);
+    }
+
+    public static long getTimestamp(String dateTime) {
+        LocalDateTime time = LocalDateTime.parse(dateTime, formatter);
+        return time.toEpochSecond(ZoneOffset.UTC);
     }
 }
--- src/main/java/org/example/Statistics.java
+++ src/main/java/org/example/Statistics.java
@@ -1,11 +1,4 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class Statistics {
-    private final int maxRequestsPerSecond;
-    private final int averageRequestsPerSecond;
+public record Statistics(int maxRequestsPerSecond, int averageRequestsPerSecond) {
 }
